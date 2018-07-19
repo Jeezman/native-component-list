ef70bc04058f16bc5f8a7da6fc30d6743e9e09f2
Jesse Ruder <jesse@expo.io>
2018-07-19 22:25:49 +0000

    Update IntentLauncher screen
    fbshipit-source-id: 5354cc5

--- screens/IntentLauncherScreen.js
+++ screens/IntentLauncherScreen.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Button, Platform, ScrollView, Text, ToastAndroid, View } from 'react-native';
+import { Platform, ScrollView, Text, ToastAndroid, View } from 'react-native';
 import { IntentLauncherAndroid } from 'expo';
+import Button from '../components/Button';
 
 export default class IntentLauncherScreen extends React.Component {
   static navigationOptions = {
     title: 'IntentLauncher',
   };
-  
-  renderSettingsLink(title, activity) {
+
+  renderSettingsLink(title, activity, data = null, uri = null) {
     if (Platform.OS !== 'android') {
       return (
         <View>
@@ -20,13 +21,14 @@ export default class IntentLauncherScreen extends React.Component {
         <Button
           onPress={async () => {
             try {
-              await IntentLauncherAndroid.startActivityAsync(activity);
+              await IntentLauncherAndroid.startActivityAsync(activity, data, uri);
               ToastAndroid.show(`Activity finished`, ToastAndroid.SHORT);
             } catch (e) {
               ToastAndroid.show(`An error occurred: ${e.message}`, ToastAndroid.SHORT);
             }
           }}
           title={title}
+          style={{ marginBottom: 10 }}
         />
       </View>
     );
@@ -38,6 +40,13 @@ export default class IntentLauncherScreen extends React.Component {
         {this.renderSettingsLink(
           'Location Settings',
           IntentLauncherAndroid.ACTION_LOCATION_SOURCE_SETTINGS
+        )}
+
+        {this.renderSettingsLink(
+          'Application Details for Play Store',
+          IntentLauncherAndroid.ACTION_APPLICATION_DETAILS_SETTINGS,
+          null,
+          'package:com.android.vending'
         )}
       </ScrollView>
     );
